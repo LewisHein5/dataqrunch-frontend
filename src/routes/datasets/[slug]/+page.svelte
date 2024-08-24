6f4fd7957f05f83a8b96e934ef761b81a884decf
Lewis Hein <lewishein5@gmail.com>
2024-08-24 17:11:38 -0500

    Checkpoint

--- src/routes/datasets/[slug]/+page.svelte
+++ src/routes/datasets/[slug]/+page.svelte
@@ -1,6 +1,25 @@
 <script>
+    import {Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell} from "flowbite-svelte";
+
 /** @type {import('./$types').PageData} */
 export let data;
 </script>
 
 <h1>{data.dataset.name}</h1>
+
+<Table>
+    <TableHead>
+        {#each data.dataset.spec[0].columns as columnSpec}
+            <TableHeadCell>{columnSpec.columnName}</TableHeadCell>
+        {/each}
+    </TableHead>
+    <TableBody>
+        {#each data.rows.rows as row}
+            <TableBodyRow>
+                {#each row.data as cell}
+                    <TableBodyCell>{cell}</TableBodyCell>
+                {/each}
+            </TableBodyRow>
+        {/each}
+    </TableBody>
+</Table>
