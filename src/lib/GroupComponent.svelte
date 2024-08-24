444864fd07a3cd3e69b99e1695c462d1c60488fb
Lewis Hein <lewishein5@gmail.com>
2024-08-24 15:46:31 -0500

    Checkpoint, start using flowbite, does not compile

--- src/lib/GroupComponent.svelte
+++ src/lib/GroupComponent.svelte
@@ -1,26 +1,36 @@
 <script lang="ts">
     import type {Group} from "$lib/dataqrunch";
-    import IconAccountBox from '~icons/mdi/account-box'
     export let group: Group;
+    export let expanded: boolean;
+    import Icon from "@iconify/svelte";  
 </script>
 
 <div class="group-container">
-    <div class="group-name"><IconAccountBox/>{group.name}</div>
-    <ul class="subgroups-list">
-        {#each group.subgroups as subgroup}
-            <li class="subgroups-item"><svelte:self group="{subgroup}"></svelte:self></li>
-        {/each}
-    </ul>
-    <ul class="dataset-list">
-        {#each group.datasets as dataset}
-            <li class="dataset-item">{dataset}</li>
-        {/each}
-    </ul>
+    <div class="group-name" on:click={() => {expanded = !expanded}}>
+        {#if expanded}
+            <Icon icon="ph:folder-open-bold"/>
+        {:else}
+            <Icon icon="ph:folder-bold" />    
+        {/if}
+        {group.name}
+    </div>
+    {#if expanded}
+        <ul class="subgroups-list">
+            {#each group.subgroups as subgroup}
+                <li class="subgroups-item"><svelte:self group="{subgroup}" expanded="{false}"></svelte:self></li>
+            {/each}
+        </ul>
+        <ul class="dataset-list">
+            {#each group.datasets as dataset}
+                <li class="dataset-item"><Icon icon="ph:file-bold" />{dataset.name}</li>
+            {/each}
+        </ul>
+    {/if}
 </div>
 
 <style>
     .group-container {
-        margin-left: 20px;
+        margin-left: min(10px, 20%);
     }
 
     .group-name {
@@ -32,21 +42,14 @@
 
     .dataset-list, .subgroups-list {
         list-style-type: none;
-        padding-left: 20px;
+        padding-left: min(10px, 20%);
         border-left: 1px solid #ccc;
         margin-left: 10px;
     }
 
     .dataset-item, .subgroups-item {
         padding: 2px 0;
-    }
-
-    .subgroups-item .group-container {
-        margin-left: 0;
-    }
-
-    .subgroups-item .group-name {
-        font-weight: bold;
+        list-style-type: none;
     }
 </style>
 
--- src/routes/datasets/+page.svelte
+++ src/routes/datasets/+page.svelte
@@ -1,12 +1,39 @@
 <script>
     import GroupComponent from "$lib/GroupComponent.svelte";
-
     /** @type {import('./$types').PageData} */
     export let data;
+    
+    import { Drawer, Button, CloseButton } from 'flowbite-svelte';
+    import { InfoCircleSolid, ArrowRightOutline } from 'flowbite-svelte-icons';
+    import { sineIn } from 'svelte/easing';
+    let hidden1 = true;
+    let transitionParams = {
+        x: -320,
+        duration: 200,
+        easing: sineIn
+    };
 </script>
 
-<ul>
-    {#each data.groups as group}
-            <li><GroupComponent group="{group}"></GroupComponent></li>
-    {/each}
-</ul>
+<div class="text-center">
+    <Button on:click={() => (hidden1 = false)}>Show drawer</Button>
+</div>
+
+<Drawer transitionType="fly" {transitionParams} bind:hidden={hidden1} id="sidebar1">
+    <div class="flex items-center">
+        <h5 id="drawer-label" class="inline-flex items-center mb-4 text-base font-semibold text-gray-500 dark:text-gray-400">
+            <InfoCircleSolid class="w-5 h-5 me-2.5" />Info
+        </h5>
+        <CloseButton on:click={() => (hidden1 = true)} class="mb-4 dark:text-white" />
+    </div>
+    <ul>
+        {#each data.groups as group}
+            <li style="list-style-type: none"><GroupComponent group="{group}" expanded="{false}"></GroupComponent></li>
+        {/each}
+    </ul>
+
+    
+    <div class="grid grid-cols-2 gap-4">
+        <Button color="light" href="/">Learn more</Button>
+        <Button href="/" class="px-4">Get access <ArrowRightOutline class="w-5 h-5 ms-2" /></Button>
+    </div>
+</Drawer>
