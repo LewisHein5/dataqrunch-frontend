7b915066e89b23bd7f12cb9350eb39e512585e97
Lewis Hein <lewishein5@gmail.com>
2024-08-24 23:00:10 -0500

    Add keyboard navigation to dataset table cells
    Handle Tab and Enter key events to navigate through table cells. Introduce functions to update focus and add new rows when enter is pressed on the last row of the table.

--- src/routes/datasets/[slug]/+page.svelte
+++ src/routes/datasets/[slug]/+page.svelte
@@ -9,9 +9,37 @@
         TableHeadCell
     } from "flowbite-svelte";
 
-/** @type {import('./$types').PageData} */
-export let data;
-let focus={x:-1, y:-1}
+    /** @type {import('./$types').PageData} */
+    export let data;
+    let focus={x:-1, y:-1}
+    let tableSize = {nCols: data.dataset.spec[0].columns.length, nRows: data.rows.rows.length}
+    
+    function handleKeyPressInTable(e){
+        if (e.keyCode === 9) {
+            handleTab(focus.y, focus.x)
+        } 
+        if (e.keyCode === 13)
+        {
+            handleEnter(focus.y, focus.x)
+        }
+    }
+    
+    function handleTab(row, col){
+        if (col < tableSize.nCols){
+            focus = {x: focus.x+1, y: focus.y}
+        }
+    }
+    
+    function handleEnter(row, _col) {
+        if (row < tableSize.nRows - 1){
+            focus = {x:0, y:focus.y+1}
+        }
+        else if (row === tableSize.nRows - 1){
+            let spec = data.dataset.spec[0];
+            data.rows.rows = [...data.rows.rows, {datasetSpecVersion: spec.version, data: spec.columns.map((_)=>{return ""})}]
+            focus = {x:0, y:focus.y+1}
+        } 
+    }
 </script>
 
 <h1>{data.dataset.name}</h1>
@@ -27,9 +55,9 @@ let focus={x:-1, y:-1}
             <TableBodyRow>
                 {#each row.data as cell, x}
                     <TableBodyCell>
-                        <div on:click={(event) => {focus = {x: x,  y: y}; console.log(focus)}} aria-describedby="Row {y} Column {x}" on:keydown={(e) => {if (e.keyCode === 9){ focus = {x: focus.x+1, y: focus.y}}}}>
+                        <div on:click={(_) => {focus = {x: x,  y: y}}} aria-describedby="Row {y} Column {x}" on:keydown={handleKeyPressInTable}>
                         {#if focus.x === x && focus.y === y}
-                            <FloatingLabelInput color="blue" style="outlined" id="outlined_success" aria-describedby="outlined_success_help" name="outlined_success" type="text">
+                            <FloatingLabelInput bind:value={row.data[x]} color="blue" style="outlined" id="outlined_success" aria-describedby="outlined_success_help" name="outlined_success" type="text">
                                 {data.dataset.spec[0].columns[x].columnName} ({data.dataset.spec[0].columns[x].dataTypes})
                             </FloatingLabelInput>
                         {:else}
