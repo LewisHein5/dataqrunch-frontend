8dd8a6fd6e6f4ba9df600f9a9fc2ea6135dddf88
Lewis Hein <lewishein5@gmail.com>
2024-10-26 11:50:23 -0500

    Update IP address (temporary)

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -26,7 +26,7 @@ export class Client{
             metadata: Metadata({Authorization: this.auth_token})
         }
         
-        this.channel = createChannel("http://146.190.58.38:10000")
+        this.channel = createChannel("http://104.248.122.69:10000")
         this.client = createClient(DataQrunchServiceDefinition, this.channel)
     }
     
